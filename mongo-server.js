edffec7172174d2d78a91b302bfd26ad7a75a1c7
Tyler Brinkman <tbrinkman3@gmail.com>
2021-07-17 15:09:47 -0500

    Modifited date in db

--- mongo-server.js
+++ mongo-server.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/qa');
+mongoose.connect('mongodb://localhost/qa', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => console.log('Connnectedto QA DB'))
+db.once('open', () => {
+
+
+  console.log('Connnectedto QA DB')
+
+  //console.log(db.db.collection('questions'))
+
+  // db.db.collection('questions', (err, collection) => {
+  //   collection.find({product_id:1}).toArray((err, data) => {
+  //     console.log(data.length)
+  //     console.log('----------------------------------')
+  //   })
+
+  // })
+
+})
+
+
+
+
 
 
 const questionSchema = new mongoose.Schema({
@@ -13,12 +32,19 @@ const questionSchema = new mongoose.Schema({
   question_id: Number,
   product_id: Number,
   question_body: String,
-  question_date: Number,
+  question_date: String,
   asker_name: String,
   asker_email: String,
   reported: Number,
   question_helpfulness: Number,
 })
+
+const Question = mongoose.model('Questinon', questionSchema, 'questions');
+
+// Question.find({product_id:1}, (err, data) => {
+//   console.log(data.length)
+// })
+
 
 const answerSchema = new mongoose.Schema({
   //id	question_id	body	date_written	answerer_name	answerer_email	reported	helpful
@@ -32,10 +58,18 @@ const answerSchema = new mongoose.Schema({
   hepfulness: Number
 })
 
-const photoSchma = new mongoose.Schema({
+const Answer = mongoose.model('Answer', answerSchema, 'answers');
+
+const photoSchema = new mongoose.Schema({
   // id answer_id url
   photo_id: Number,
   answer_id: Number,
   url: String
 })
 
+const Answer_Photos = mongoose.model('Answer_Photos', photoSchema, 'photos');
+
+
+Answer_Photos.find({answer_id:5}, (err, data) => {
+  console.log(data)
+})
