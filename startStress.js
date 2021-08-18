dfcd916f9f9cfa4035cc1101eabe78ed40311920
Tyler Brinkman <tbrinkman3@gmail.com>
2021-08-17 20:33:19 -0500

    Start of scaling

--- startStress.js
+++ startStress.js
@@ -1,11 +1,11 @@
 const app = require('./servers/mongo/server-mongo.js');
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/qaStress', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, autoIndex: false});
+mongoose.connect('mongodb://mongo-db:27017/qaStress', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, autoIndex: false});
 
 
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.listen(PORT, () => {console.log(`Listening at ${PORT}`)})
 
